867d97cf153e93e7245b1dfaaee36b376a507e5d
Odeclas Onarres <odeclasonarres@gmail.com>
2016-10-05 17:03:48 +0200

    monstrus

--- Napakalaki/src/napakalaki/PruebaNapakalaki.java
+++ Napakalaki/src/napakalaki/PruebaNapakalaki.java
@@ -5,16 +5,25 @@
  */
 package napakalaki;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author OdeclasOnarres
  */
 public class PruebaNapakalaki {
-
+    static ArrayList<Monster> monstruos = new ArrayList();
+    
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        BadConsequence badConsequence = new BadConsequence("ierdes 5 niveles y 3 tesoros visibles", 5, 3, 0);
+        Prize prize = new Prize(4,2);
+        monstruos.add(new Monster("El rey de rosa", 13,prize, badConsequence));
+      
+        
        Prize p= new Prize(1, 1);
        BadConsequence bc=new BadConsequence("dddd", 1, 1, 1);
        Monster m=new Monster("prueba", 0, p, bc);
